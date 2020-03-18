eec7aba1e486a06452314468def6be16458b9855
Ulrich Berthold <ub@artfacts.net>
2020-03-18 13:41:06 +0100

    nop, it's in the http submodule

--- flask_oauthlib/client.py
+++ flask_oauthlib/client.py
@@ -15,7 +15,7 @@ from copy import copy
 from functools import wraps
 from oauthlib.common import to_unicode, PY3, add_params_to_uri
 from flask import request, redirect, json, session, current_app
-from werkzeug import parse_options_header
+from werkzeug.http import parse_options_header
 from werkzeug.urls import url_quote, url_decode, url_encode
 from werkzeug.utils import cached_property
 from .utils import to_bytes
